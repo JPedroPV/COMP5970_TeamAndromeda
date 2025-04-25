78ae64b1312946e63c0b17da85716ff5eb11f0ae
Aiden Townsend <aidenticklesgood@gmail.com>
2025-04-25 09:09:46 -0500

    changing login button to <button> from <a>

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.teamandromeda.andromeda',
+  appName: 'Andromeda',
+  webDir: 'dist'
+};
+
+export default config;
